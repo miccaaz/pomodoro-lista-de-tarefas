e0983931e0536900b3d90f7e1091d622bb2c5f93
Miguel <mclcastroloureiro@gmail.com>
2026-01-29 16:34:06 -0300

    Removendo conponentes desnecessários

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,6 @@ const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 
-const banner = document.querySelector('.app__image')
-const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
 
 const startPauseBt = document.querySelector('#start-pause')
@@ -57,26 +55,6 @@ function alterarContexto(contexto) {
         contexto.classList.remove('active')
     }) 
     html.setAttribute('data-contexto', contexto)
-    banner.setAttribute('src', `/assets/images/${contexto}.png`)
-    switch (contexto) {
-        case "foco":
-            titulo.innerHTML = `
-            Aumente sua produtividade,<br>
-                <strong class="app__title-strong">foque no que importa.</strong>
-            `
-            break;
-        case "descanso-curto":
-            titulo.innerHTML = `
-            Que tal dar um cochilo? <strong class="app__title-strong">Faça uma pequena pausa!</strong>
-            ` 
-            break;
-        case "descanso-longo":
-            titulo.innerHTML = `
-            Já produziu demais! <strong class="app__title-strong">Que tal descansar mais.</strong>
-            `
-        default:
-            break;
-    }
 }
 
 const contagemRegressiva = () => {
